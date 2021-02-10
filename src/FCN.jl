00b7386cc6345e88ec4e7c4f19f3d9d2d934129d
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-02-10 23:43:04 +0300

    Update FCN.jl

--- src/FCN.jl
+++ src/FCN.jl
@@ -9,11 +9,11 @@ using Base.Iterators: flatten
 using Plots
 
 
-include("./conv.jl"); #export Conv
-include("./dense.jl"); #export Dense
-include("./genericmlp.jl"); #export GCN
-include("./gcn.jl"); #export GenericMLP
-include("./network_ops.jl"); #export nll4, accuracy4, train_epoch!, train_generic!
+include("./conv.jl"); export Conv
+include("./dense.jl"); export Dense
+include("./genericmlp.jl"); export GCN
+include("./gcn.jl"); export GenericMLP
+include("./network_ops.jl"); export nll4, accuracy4, train_epoch!
 
 
 end # module
