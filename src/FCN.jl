1ff5d45ce4dc219095765a825ab1308a608307ad
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-02-10 22:22:56 +0300

    Update FCN.jl

--- src/FCN.jl
+++ src/FCN.jl
@@ -11,11 +11,11 @@ using Plots
 using MAT
 
 
-include("./network/conv.jl"); #export Conv
-include("./network/dense.jl"); #export Dense
-include("./network/genericmlp.jl"); #export GCN
-include("./network/gcn.jl"); #export GenericMLP
-include("./network/network_ops.jl"); #export nll4, accuracy4, train_epoch!, train_generic!
+include("./conv.jl"); #export Conv
+include("./dense.jl"); #export Dense
+include("./genericmlp.jl"); #export GCN
+include("./gcn.jl"); #export GenericMLP
+include("./network_ops.jl"); #export nll4, accuracy4, train_epoch!, train_generic!
 
 
 end # module
